50bae382bf18d3c835e343dd09438c92d3098dec
Christopher S. Buttars <chrisbuttars@live.com>
2024-10-24 11:45:28 -0500

    Enbaled the viewing of hidden files in neo-tree for Neovim. Creates 2 new hyprland configuration files and sourced them back into hyprland.conf.

--- .config/nvim/lua/plugins/lazyvim-overrides.lua
+++ .config/nvim/lua/plugins/lazyvim-overrides.lua
@@ -23,6 +23,19 @@ local returnValue = {
       colorscheme = "catppuccin",
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      },
+    },
+  },
 }
 
 return returnValue
